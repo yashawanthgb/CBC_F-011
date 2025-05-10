3e4decd6ad14ff5252d46ee04b8bafb14e6a06ae
yashawanthgb <yashawanthgb@gmail.com>
2025-05-10 17:38:57 +0530

    first commit

---
+++ Hackthon/main.js
@@ -0,0 +1,9 @@
+const startBtn = document.getElementById("start");
+startBtn.addEventListener("click", () => {
+    window.location.href = "trail.html";
+});
+
+const bookingBtn = document.getElementById("booking");
+bookingBtn.addEventListener("click", () => {
+    window.location.href = "booking.html";
+});
